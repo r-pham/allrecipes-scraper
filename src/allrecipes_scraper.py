08f45c55381f223d75bb1bc0cd5d7538c0a6c150
Ryan Pham <phamwhich@gmail.com>
2023-01-29 19:59:13 -0800

    Set foundation for AllRecipes_Scraper class (#1)

--- src/allrecipes_scraper.py
+++ src/allrecipes_scraper.py
@@ -1,8 +1,11 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
+import re
+import requests
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from typing import List
 
+# TODO: Fill these with page url's
 RECIPE_CATEGORY_PAGES = {
     "QUICK AND EASY DINNERS FOR ONE": "",
     "COOKING FOR TWO": "",
@@ -21,11 +24,26 @@ RECIPE_CATEGORY_PAGES = {
 
 @dataclass_json
 @dataclass
+class RecipeDetail:
+    label: str
+    value: str
+
+
+@dataclass_json
+@dataclass
+class RecipeIngredient:
+    quantiy: int
+    unit: str
+    name: str
+
+
+@dataclass_json
+@dataclass
 class Recipe:
     name: str
-    details: str
-    ingredients: str
-    directions: str
+    details: List[RecipeDetail]
+    ingredients: List[RecipeIngredient]
+    directions: List[str]
 
 
 class AllRecipes_Scraper:
@@ -33,23 +51,40 @@ class AllRecipes_Scraper:
     Main class that allows us to scrape recipes and details from AllRecipes website
     """
 
-    HOME_URL = "https://www.allrecipes.com/"
-
-    def get_recipe(url: str) -> Recipe:
-        """_summary_
+    def get_recipe(self, url: str) -> Recipe:
+        """
+        Return recipe details from a page
 
         Args:
             url (str): _description_
         """
-        # TODO: Create a dataclass to be returned
-        # Scrape contents from URL and consolidate everything into a uniform class
-        raise NotImplementedError
+        if not self._validate_recipe_url(url):
+            return
+        # Get page HTML
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        # Parse recipe details, ingredients, and directions
+        recipe_name = soup.find(id="article-heading_2-0")
+        recipe_details = self._get_recipe_details(soup.find(id="recipe-details_1-0"))
+        ingredients = self._get_recipe_ingredients(
+            soup.find(id="mntl-structured-ingredients_1-0")
+        )
+        directions = self._get_recipe_directions(
+            soup.find(id="recipe__steps-content_1-0")
+        )
+        return Recipe(
+            name=recipe_name.text.strip(),
+            details=recipe_details,
+            ingredients=ingredients,
+            directions=directions,
+        )
 
     def get_recipes(category: str) -> List[Recipe]:
-        """_summary_
+        """
+        Returns a list of recipes from specified category
 
         Args:
-            category (str): _description_
+            category (str): Food category to get recipes from
         """
         # Iterate through all urls from a category's main page
         # For each url, return recipe and details
@@ -60,25 +95,92 @@ class AllRecipes_Scraper:
         Return all image links found on a recipe page
 
         Args:
-            url (str): _description_
+            url (str): Recipe page URL
         """
         raise NotImplementedError
 
-    def _get_all_recipe_urls(url: str):
+    @staticmethod
+    def _validate_recipe_url(url: str) -> bool:
+        """
+        Ensure that the specified url is a valid recipe page
+
+        Args:
+            url (str): _description_
+        """
+        # TODO: Look into optimizing this -- I feel like either pattern can be simplified or regex not needed
+        return bool(
+            re.match("^https:\/\/www\.allrecipes\.com\/recipe\/\d+\/.*\/$", url)
+        )
+
+    def _get_all_recipe_urls(self, url: str) -> List[str]:
         """
         Return all recipe urls found on a page
 
         Args:
             url (str): URL of a page to get other urls
         """
-        # Ensure that all urls follow this format: https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/
-        raise NotImplementedError
+        links = []
+        page = requests.get(url)
+        soup = BeautifulSoup(page, "html.parser")
 
-    def _html_to_recipe(html: str) -> Recipe:
+        for link in soup.find_all("a", href=True):
+            if self._validate_recipe_url(link["href"]):
+                links.append(link["href"])
+        return links
+
+    @staticmethod
+    def _get_recipe_details(recipe_content_html: element.Tag) -> List[RecipeDetail]:
         """
-        Cleans up HTML to be parsed as Recipe object
+        Get recipe details from a recipe page
 
         Args:
-            html (str): HTML to be returned by request
+            recipe_content_html (element.Tag): Recipe page HTML for recipe details
         """
-        raise NotImplementedError
+        details = []
+        detail_elements = recipe_content_html.find_all(
+            "div", class_="mntl-recipe-details__item"
+        )
+        for e in detail_elements:
+            cleaned_element = e.text.strip().split("\n")
+            details.append(
+                RecipeDetail(
+                    label=cleaned_element[0].replace(":", ""), value=cleaned_element[1]
+                )
+            )
+        return details
+
+    @staticmethod
+    def _get_recipe_ingredients(ingredients_content_html: element.Tag) -> List[str]:
+        """
+        Get recipe ingredients from a recipe page
+
+        Args:
+            ingredients_content_html (element.Tag): Recipe page HTML for ingredients
+        """
+        # TODO: Split elements to appropriate Ingredient attributes and return Ingredient data class
+        ingredients = []
+        ingredient_elements = ingredients_content_html.find_all(
+            "li", class_="mntl-structured-ingredients__list-item"
+        )
+        for e in ingredient_elements:
+            cleaned_element = e.text.strip()
+            ingredients.append(cleaned_element)
+        return ingredients
+
+    @staticmethod
+    def _get_recipe_directions(directions_content_html: element.Tag) -> List[str]:
+        """
+        Get recipe directions from a recipe page
+
+        Args:
+            directions_content_html (element.Tag): Recipe page HTML for directions
+        """
+        # TODO: Get directions in ordered list
+        directions = []
+        direction_elements = directions_content_html.find_all(
+            "p", class_="comp mntl-sc-block mntl-sc-block-html"
+        )
+        for e in direction_elements:
+            cleaned_element = e.text.strip()
+            directions.append(cleaned_element)
+        return directions
